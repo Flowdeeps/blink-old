fe1c510ac291713b8d7d348bcc47fb469742baea
Pete Haughie <pete.haughie@sourcefabric.org>
2016-06-02 15:31:47 +0100

    I've added two ways of reintroducing the blink tag because I'm a monster

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,18 @@
 window.onload = function() {
-	
+
+	var addBadBlink = function() {
+
+		var body = document.getElementsByTagName( "body" ),
+				bodyText = body[0].innerHTML,
+				blink1 = /<blink>/g,
+				blink2 = /<\/blink>/g;
+
+				bodyTextNew = bodyText.replace( blink1, "<span class=\"blink\">").replace( blink2, "</span>");
+
+				console.log(bodyText, bodyTextNew);
+
+				body[0].innerHTML = bodyTextNew;
+
+	}();
+
 };
