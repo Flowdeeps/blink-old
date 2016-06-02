c8db34e1b2617befa81b474139e2c4cd2da24b0b
Flowdeeps <speedwolf@gmail.com>
2016-06-02 20:02:08 +0100

    Added a second less evil method of making the page blink

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,5 +1,10 @@
 window.onload = function() {
 
+	// To get either of these to work you'll have to enable one of the functions below
+	// moral dilema
+	// addGoodBLink();
+	// addBadBlink();
+
 	var addBadBlink = function() {
 
 		var body = document.getElementsByTagName( "body" ),
@@ -9,10 +14,34 @@ window.onload = function() {
 
 				bodyTextNew = bodyText.replace( blink1, "<span class=\"blink\">").replace( blink2, "</span>");
 
-				console.log(bodyText, bodyTextNew);
-
 				body[0].innerHTML = bodyTextNew;
 
-	}();
+	}
+
+	var addGoodBlink = function() {
+
+		var blinks = document.getElementsByTagName( "blink" ),
+				blinking = false;
+
+		setInterval (function() {
+			blinkElems();
+		}, 1000);
+
+		var blinkElems = function() {
+			if ( blinking == false ) {
+				blinking = true;
+				for ( i = 0; i < blinks.length ; i++ ) {
+					blinks[i].setAttribute( "class", "-_-" );
+				}
+			} else {
+				blinking = false;
+				for ( i = 0; i < blinks.length ; i++ ) {
+					blinks[i].classList.remove( "-_-" );
+				}
+			}
+			console.log(blinking);
+		}
+
+	};
 
 };
